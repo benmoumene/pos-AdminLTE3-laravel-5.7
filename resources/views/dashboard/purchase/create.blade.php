6fe4f2d1ea62368a36eb4e3a6b98b08181832f11
Meghlaoui TouwfiQ <touwfiqdev@gmail.com>
2019-12-03 23:40:31 +0100

    fix #2 update provider and client modals in sale and purchase views with new fields

--- resources/views/dashboard/purchase/create.blade.php
+++ resources/views/dashboard/purchase/create.blade.php
@@ -135,26 +135,49 @@
                             @include('partials._errors')
                             <div class="modal-body">
                                 <div class="row">
-                                    <div class="col-md-12">
+                                    <div class="col-md-6">
                                         <div class="form-group">
                                             <label>@lang('site.providername')</label>
-                                            <input type="text" name="provider_name" id="provider_name"
-                                                class="form-control" value="">
+                                            <input type="text" name="provider_name" id="" class="form-control"
+                                                value="{{ old('provider_name') }}">
                                         </div>
                                         <div class="form-group">
                                             <label>@lang('site.phone')</label>
-                                            <input type="text" name="phone" id="phone" class="form-control" value="">
+                                            <input type="text" name="phone" id="" class="form-control"
+                                                value="{{ old('phone') }}">
                                         </div>
                                         <div class="form-group">
                                             <label>@lang('site.address')</label>
-                                            <textarea type="text" name="address" id="address"
-                                                class="form-control"></textarea>
+                                            <textarea type="text" name="address" id=""
+                                                class="form-control">{{ old('address') }}</textarea>
 
                                         </div>
                                         <div class="form-group">
                                             <label>@lang('site.description')</label>
-                                            <textarea type="text" name="description" id="description"
-                                                class="form-control"></textarea>
+                                            <textarea type="text" name="description" id=""
+                                                class="form-control">{{ old('description') }}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>@lang('site.numeroregistrecommerce')</label>
+                                            <input type="text" name="rc" id="" class="form-control"
+                                                value="{{ old('rc') }}">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>@lang('site.numeroarticle')</label>
+                                            <input type="number" name="article" id="" class="form-control"
+                                                value="{{ old('article') }}">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>@lang('site.nif')</label>
+                                            <input type="number" name="nif" id="" class="form-control"
+                                                value="{{ old('nif') }}">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>@lang('site.nis')</label>
+                                            <input type="number" name="nis" id="" class="form-control"
+                                                value="{{ old('nis') }}">
                                         </div>
                                     </div>
                                 </div>
--- resources/views/dashboard/sale/create.blade.php
+++ resources/views/dashboard/sale/create.blade.php
@@ -131,26 +131,48 @@
                             @include('partials._errors')
                             <div class="modal-body">
                                 <div class="row">
-                                    <div class="col-md-12">
+                                    <div class="col-md-6">
                                         <div class="form-group">
                                             <label>@lang('site.clientname')</label>
-                                            <input type="text" name="client_name" id="client_name" class="form-control"
-                                                value="">
+                                            <input type="text" name="client_name" id="" class="form-control"
+                                                value="{{ old('client_name') }}">
                                         </div>
                                         <div class="form-group">
                                             <label>@lang('site.phone')</label>
-                                            <input type="text" name="phone" id="phone" class="form-control" value="">
+                                            <input type="text" name="phone" id="" class="form-control"
+                                                value="{{ old('phone') }}">
                                         </div>
                                         <div class="form-group">
                                             <label>@lang('site.address')</label>
-                                            <textarea type="text" name="address" id="address"
-                                                class="form-control"></textarea>
-
+                                            <textarea type="text" name="address" id=""
+                                                class="form-control">{{ old('address') }}</textarea>
                                         </div>
                                         <div class="form-group">
                                             <label>@lang('site.description')</label>
-                                            <textarea type="text" name="description" id="description"
-                                                class="form-control"></textarea>
+                                            <textarea type="text" name="description" id=""
+                                                class="form-control">{{ old('description') }}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>@lang('site.numeroregistrecommerce')</label>
+                                            <input type="text" name="rc" id="" class="form-control"
+                                                value="{{ old('rc') }}">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>@lang('site.numeroarticle')</label>
+                                            <input type="number" name="article" id="" class="form-control"
+                                                value="{{ old('article') }}">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>@lang('site.nif')</label>
+                                            <input type="number" name="nif" id="" class="form-control"
+                                                value="{{ old('nif') }}">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>@lang('site.nis')</label>
+                                            <input type="number" name="nis" id="" class="form-control"
+                                                value="{{ old('nis') }}">
                                         </div>
                                     </div>
                                 </div>
